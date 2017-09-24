6ce2b914047b617901b9718dd5f744eeb315ccfd
Anthony Sneed <tony@tonysneed.com>
2017-09-23 20:35:35 -0500

    Enabled doc comments.

--- TrackableEntities.EF.Core/NavigationExtensions.cs
+++ TrackableEntities.EF.Core/NavigationExtensions.cs
@@ -3,8 +3,16 @@ using Microsoft.EntityFrameworkCore.Metadata;
 
 namespace TrackableEntities.EF.Core
 {
+    /// <summary>
+    /// Extension methods for INavigation.
+    /// </summary>
     public static class NavigationExtensions
     {
+        /// <summary>
+        /// Infer relationship type from an INavigation.
+        /// </summary>
+        /// <param name="nav">Navigation property which can be used to navigate a relationship.</param>
+        /// <returns>Type of relationship between entities; null if INavigation is null.</returns>
         public static RelationshipType? GetRelationshipType(this INavigation nav)
         {
             if (nav == null) return null;

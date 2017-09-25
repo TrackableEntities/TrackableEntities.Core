fb98d57ecefa47a25b99efdfc10f2a66f5c9e4aa
Anthony Sneed <tony@tonysneed.com>
2017-09-24 20:45:01 -0500

    Add IMergeable interface to Common.Core.

---
+++ TrackableEntities.Common.Core/IMergeable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TrackableEntities.Common.Core
+{
+    /// <summary>
+    /// Provides an EntityIdentifier for correlation when merging changes.
+    /// </summary>
+    public interface IMergeable
+    {
+        /// <summary>Identifier used for correlation with MergeChanges.</summary>
+        Guid EntityIdentifier { get; set; }
+    }
+}

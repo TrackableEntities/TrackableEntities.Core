4476e720452036982ed031c1c54f833e71986b55
Anthony Sneed <tony@tonysneed.com>
2017-09-27 11:36:58 -0500

    Fix Release Notes link in NuGet package. - Also update nuget-symbols.cmd.

--- NuGet/nuget-symbols.cmd
+++ NuGet/nuget-symbols.cmd
@@ -1,8 +1,9 @@
 rem Pushing Symbols packages to NuGet.org
 
-set version=1.0.0-alpha2
+set version=1.0.0-beta
+set source=https://nuget.smbsrc.net/
 NuGet.exe SetApiKey 5682793e-2994-4016-b7b4-c11be576703b
-NuGet.exe push TrackableEntities.Common.Core\TrackableEntities.Common.Core.%version%.symbols.nupkg
-NuGet.exe push TrackableEntities.EF.Core\TrackableEntities.EF.Core.%version%.symbols.nupkg
-rem NuGet.exe push Output\TrackableEntities.Patterns.Core\TrackableEntities.Patterns.Core.%version%.symbols.nupkg
-rem NuGet.exe push Output\TrackableEntities.Patterns.EF.Core\TrackableEntities.Patterns.EF.Core.%version%.symbols.nupkg
+rem NuGet.exe push TrackableEntities.Common.Core\TrackableEntities.Common.Core.%version%.symbols.nupkg -source %source%
+NuGet.exe push TrackableEntities.EF.Core\TrackableEntities.EF.Core.%version%.symbols.nupkg -source %source%
+rem NuGet.exe push Output\TrackableEntities.Patterns.Core\TrackableEntities.Patterns.Core.%version%.symbols.nupkg -source %source%
+rem NuGet.exe push Output\TrackableEntities.Patterns.EF.Core\TrackableEntities.Patterns.EF.Core.%version%.symbols.nupkg -source %source%
